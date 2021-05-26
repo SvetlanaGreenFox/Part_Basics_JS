82bf43c674ca5a363c98fbc60e297008cb128e1d
Svetlana Lisina <svetlanagreenfox@gmail.com>
2021-05-26 17:56:05 +0300

    Решение первого задания

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,7 +1,23 @@
+'use strict';
+
 function solveEquation(a, b, c) {
   let arr;
   // код для задачи №1 писать здесь
-  return arr; // array
+  let discriminantis = Math.pow(b, 2) - (4 * a * c);
+
+  if (discriminantis > 0) {
+    let rootOfDiscriminantis = Math.sqrt(discriminantis);
+    let valueOne = ((-(b)) + rootOfDiscriminantis) / (2 * a);
+    let valueTwo = ((-(b)) - rootOfDiscriminantis) / (2 * a);
+
+    return arr.push(valueOne, valueTwo);
+  } else if (discriminantis === 0) {
+    let value = (-(b)) / (2 * a)
+
+    return arr.push(value);
+  } else {
+    return arr;
+  }
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
