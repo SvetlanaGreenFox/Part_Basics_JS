1412bc6a01e63c717be4e8bc44a7a89878f7700e
Svetlana Lisina <svetlanagreenfox@gmail.com>
2021-05-27 15:24:32 +0300

    Решение Задачи 2

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -23,8 +23,32 @@ function solveEquation(a, b, c) {
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
   let totalAmount;
+  let percentCredit = Number(percent / 100);
+  let contributionCredit = Number(contribution);
+  let amountCredit = Number(amount);
 
-  // код для задачи №2 писать здесь
+  let creditPeriod = numberMonths(date);
+  let p = percentCredit / 12;
 
+  let bodyCredit = amountCredit - contributionCredit;
+  let creditPayment = bodyCredit * (p + p / (((1 + p) ** creditPeriod) - 1));
+  // totalAmount = (contributionCredit + (creditPayment * creditPeriod)).toFixed(2));
+
+  let freeAmount = amountCredit / creditPeriod;
+  let freePercent = creditPayment - freeAmount;
+  let allPercent = freePercent * creditPeriod;
+
+  totalAmount = Number((amountCredit + allPercent).toFixed(2));
+
+  console.log(totalAmount);
   return totalAmount;
+}
+
+function numberMonths(date) {
+  let now = new Date();
+  let finishCredit = new Date(date);
+  let fullYear = 12 * (finishCredit.getFullYear() - now.getFullYear());
+  let numberMonths = finishCredit.getMonth() - now.getMonth() + fullYear;
+
+  return numberMonths;
 }
