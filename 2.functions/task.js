5933b91cc22e9ca519825e910008717e4ed8d747
Svetlana Lisina <svetlanagreenfox@gmail.com>
2021-06-01 15:52:23 +0300

    Task completed

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,26 +1,53 @@
 // Задание 1
+
 function getArrayParams(arr) {
-  let min,max,sum,avg;
+  let max = arr[0];
+  let min = arr[0];
+  let sum = 0;
+  let avg = 0;
 
-  // Ваш код
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+    avg = (sum / arr.length).toFixed(2);
+  }
 
-  return { min:min, max:max, avg:avg };
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
+    } if (arr[i] < min) {
+      min = arr[i];
+    }
+  }
+
+  avg = +avg
+
+  return { min: min, max: max, avg: avg };
 }
 
-// Задание 2
-function worker(arr) {
-  let sum;
 
-  // Ваш код
+// Задание 2
+
+function worker(arr) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
 
   return sum;
 }
 
 function makeWork(arrOfArr, func) {
-  let max
+  let max = func(arrOfArr[0]);
 
-  // Ваш кода
-  // for ...
-  
-  return max
+
+  for (let i = 1; i < arrOfArr.length; i++) {
+
+    if (func(arrOfArr[i]) > max) {
+      max = func(arrOfArr[i]);
+
+    }
+  }
+
+  return max;
 }
+
