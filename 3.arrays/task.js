f9e0446b442106ad4d5deb2a0d073adabc0a3159
Svetlana Lisina <svetlanagreenfox@gmail.com>
2021-06-03 18:02:20 +0300

    Task completed

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,23 @@
+//Задание 1
 function compareArrays(arr1, arr2) {
-  let result;
+  let result = arr1.every(isIdentical);
 
-  // Ваш код
+  function isIdentical {
+    if (arr1.length === arr2.length) {
+      return item === arr2[idx];
+    }
+  }
 
   return result; // boolean
 }
 
-function advancedFilter(arr) {
-  let resultArr;
 
-  // Ваш код
+//Задание 2
+function advancedFilter(arr) {
+
+  let resultArr = arr.filter((item) => item > 0 && item % 3 === 0);
+
+  resultArr = resultArr.map(item => item * 10);
 
   return resultArr; // array
 }
