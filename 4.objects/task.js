e5522dce5025294f428082ac406068c9295638dc
Svetlana Lisina <svetlanagreenfox@gmail.com>
2021-06-17 22:06:47 +0300

    Task added

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,42 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+  // Ваш код
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
 }
 
 Student.prototype.setSubject = function (subjectName) {
   //ваш код
+  this.subject = subjectName;
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function (mark) {
+  if (this.marks === undefined) {
+    this.marks = [];
+    this.marks.push(mark);
+  }
+}
+
+Student.prototype.addMarks = function (...marks) {
+  for (let i = 0; i < marks.length; i++) {
+    this.marks.push(marks[i]);
+  }
+}
+
+
+Student.prototype.getAverage = function () {
+  let count = 0;
+  let average = count / this.marks.length;
+  for (let i = 0; i < this.marks.length; i++) {
+    count = count + this.marks[i];
+  }
+  return average;
+}
+
+Student.prototype.exclude = function (reason) {
+  delete this.subject;
+  delete this.marks;
+
+  this.reason = reason;
+}
+
